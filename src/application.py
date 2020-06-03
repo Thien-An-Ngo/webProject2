7fcbaa69d997741feafd9fd85ebd2638525be72b
Thien An Ngo <>
2020-06-03 19:33:15 +0200

    adding channels, errors, and fetching existing channels as well as messages

--- src/application.py
+++ src/application.py
@@ -1,13 +1,45 @@
 import os
 
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, request, jsonify, redirect
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+channels = []
+chats = []
+global channelCount
+error = ""
+channelCount = 0
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", error=error, channels=channels)
+
+@app.route("/messages/<int:id>")
+def messages(id):
+    return jsonify({"messages": chats[id]})
+
+@app.route("/channels")
+def getChannels():
+    return jsonify({"channels": channels})
+
+@app.route("/addChannel", methods=["POST"])
+def addChannel():
+    global error
+    error = ""
+    channelName = request.form.get('channelName')
+    if len(channelName) < 3 or len(channelName) > 12:
+        error = "Your channel has to contain a minimum of 3 and a maximum of 12 letters."
+        return redirect('/')
+    for channel in channels:
+        if channelName == channel["channelName"]:
+            error = "Channel name is already taken."
+            return redirect('/')
+    channelCount = len(channels)
+    channelElement = {"channelName": f"{channelName}", "id": channelCount}
+    channels.append(channelElement)
+    messageList = []
+    chats.append(messageList)
+    return redirect('/')
--- src/static/script.js
+++ src/static/script.js
@@ -1,4 +1,46 @@
+const handleClickAddChannel = () => {
+
+}
+
+const handleClickOnChannel = (id) => {
+    console.log('click on: ', id);
+    const chatroom = document.getElementById('chat');
+    chatroom.innerHTML = ""
+    const request = new XMLHttpRequest();
+    request.open('GET', `/messages/${id}`);
+    request.onload = () => {
+        const messages = JSON.parse(request.response);
+        if (!messages) {
+            return chatroom.innerHTML = "";
+        };
+        console.log(`messages: ${messages}`);
+        messages.messages.forEach(message => {
+            const content = document.createElement('div');
+            content.classList.add('column' + 'my-5');
+            let info = document.createElement('div');
+            info.classList.add('d-flex' + 'row' + 'justify-content-between' + 'mx-1');
+            let user = document.createElement('h6');
+            let time = document.createElement('span');
+            time.classList.add('text-muted');
+            user.innerHTML = message.user;
+            time.innerHTML = message.time;
+            info.appendChild(user);
+            info.appendChild(time);
+            content.appendChild(info);
+
+            let text = document.createElement('p');
+            text.innerHTML = message.text;
+            content.appendChild(text);
+            chatroom.appendChild(content);
+        })
+    };
+    request.send();
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+    // Frage server nach Channel für user
+    // Liste der  Channels durch gehen und darstellen
+
     while (!localStorage.getItem('username')) {
         let username = null;
         while (!username) { // warte so lange bis was eingegeben wird
@@ -6,7 +48,6 @@ document.addEventListener('DOMContentLoaded', () => {
             if (!username) {alert("Please type in your name!")}
         };
         username = username.trim();
-        console.log(`username: \'${username}\'`);
         while (username.length < 3 || username.length > 14) {
             username = null
             alert("Your username has to contain a minimum of 3 and a maximum of 14 letters! Whitespace does not count!");
@@ -24,7 +65,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const username = localStorage.getItem('username');
     const userNameEle = document.querySelectorAll('.username');
     userNameEle[0].innerHTML = username;
-    console.log(userNameEle[0]);
     
-    //.map(item => console.log('hurzel'));
+    
 })
